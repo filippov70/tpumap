ccd8fcbb63261198ed7a72a3559d887269d75cd5
filippov70 <filippov70@gmail.com>
2016-01-27 16:39:31 +0600

    добавлены тестовые данные yandex-подложка переключатель подложек

--- public_html/map.js
+++ public_html/map.js
@@ -5,19 +5,72 @@
  */
 
 $(document).ready(function () {
+
+    var buildingsStyle = {
+        'color': 'black',
+        'fillOpacity': 0.9,
+        'fillColor': '#6EB844',
+        'weight': 1.5
+    };
+
+    var enters = new L.geoJson();
+
+    var tpuBuildings = new L.geoJson(null, buildingsStyle);
+
+    $.ajax({
+        dataType: "json",
+        url: "data/buildings.geojson",
+        success: function (data) {
+            $(data.features).each(function (key, data) {
+                tpuBuildings.addData(data);
+
+            });
+        }
+    }).error(function () {});
+
+    $.ajax({
+        dataType: "json",
+        url: "data/enterstpu.geojson",
+        success: function (data) {
+            $(data.features).each(function (key, data) {
+                enters.addData(data);
+                console.log(data, key);
+            });
+        }
+    }).error(function () {});
+
+    var osm = new L.TileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            {
+                attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+                opacity: 0.7,
+                visible: true
+            }
+    );
     
-    var tpuBuildings = null;
-    
+    var yandex = new L.Yandex('hybrid', {visible: false});
+
     var map = new L.Map('map', {
         layers: [
-            new L.TileLayer(
-                    'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    {
-                        attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-                    }
-            )
+            osm,
+            yandex
         ]
     });
 
+    tpuBuildings.addTo(map);
+    enters.addTo(map);
+
     map.setView([56.46, 84.95], 15);
+
+    var baseMaps = {
+        "подложка OSM": osm,
+        "подложка Yandex": yandex
+    };
+
+//    var overlayMaps = {
+//        "Cities": cities
+//    };
+
+    L.control.layers(baseMaps).addTo(map);
+
 });
